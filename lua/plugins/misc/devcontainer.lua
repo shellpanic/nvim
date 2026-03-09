fbfccee44b00ecdf8f1cdf20caa0056287fa5b4d
fla <shell@panic.com>
2026-03-09 14:00:06 +0100

    first iteration

--- lua/plugins/misc/devcontainer.lua
+++ lua/plugins/misc/devcontainer.lua
@@ -1,12 +1,20 @@
 return {
    "https://github.com/esensar/nvim-dev-container",
-   lazy = false,
+   -- Load on demand to avoid side effects during healthcheck/startup
+   lazy = true,
+   cmd = {
+      "DevcontainerBuild",
+      "DevcontainerOpen",
+      "DevcontainerAttach",
+      "DevcontainerClean",
+   },
    dependencies = {
       "nvim-treesitter/nvim-treesitter",
    },
    opts = {
       nvim_install_as_root = false,
-      log_level = "info",
+      -- keep logs quieter to reduce writes in constrained environments
+      log_level = "warn",
       cache_images = true,
       attach_mounts = {
          neovim_config = { enabled = true },
