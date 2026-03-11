64b62213af2451e2389671bf91b51ae0cec3b5e0
fla <shell@panic.com>
2026-03-11 09:26:02 +0100

    fix treesitter

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -3,34 +3,37 @@ return {
       "nvim-treesitter/nvim-treesitter",
       event = "VeryLazy",
       build = ":TSUpdate",
-      config = function()
-         require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-               "bash",
-               "lua",
-               "python",
-               "rust",
-               "html",
-               "vue",
-               "json",
-               "jsonc",
-               "yaml",
-               "markdown",
-               "typescript",
-               "javascript",
-               "diff",
-            },
-            sync_install = false,
-            auto_install = false,
-            ignore_install = {},
-            highlight = { enable = true, disable = {}, additional_vim_regex_highlighting = false },
-            modules = {},
-          })
+      main = "nvim-treesitter.configs",
+      opts = {
+         ensure_installed = {
+            "bash",
+            "lua",
+            "python",
+            "rust",
+            "html",
+            "vue",
+            "json",
+            "jsonc",
+            "yaml",
+            "markdown",
+            "typescript",
+            "javascript",
+            "diff",
+         },
+         sync_install = false,
+         auto_install = false,
+         ignore_install = {},
+         highlight = { enable = true, disable = {}, additional_vim_regex_highlighting = false },
+         modules = {},
+      },
+      init = function()
          -- Folding powered by Treesitter
          vim.opt.foldenable = false
          vim.opt.foldmethod = "expr"
          vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-         vim.treesitter.language.register("bash", "zsh")
+         pcall(function()
+            vim.treesitter.language.register("bash", "zsh")
+         end)
       end,
    },
    {
